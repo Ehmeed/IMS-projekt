d1097759e622f68aba468ff96e847547c09659cb
Hruban Milan <xhruba08@merlin.fit.vutbr.cz>
2017-12-01 11:55:50 +0100

    added stupid header file

---
+++ model.h
@@ -0,0 +1,15 @@
+/*
+ * File model.h
+*/
+
+
+int getTod();
+void parseArgs();
+
+class Ticket;
+class SupportWorker;
+class BackendWorker;
+class CustomerRequirement;
+class BreakdownGenerator;
+class Generator;
+
