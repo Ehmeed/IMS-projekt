432d4b6b1a8ff23e897d941dfb3bc4708f340be8
Hruban Milan <xhruba08@merlin.fit.vutbr.cz>
2017-12-02 21:33:46 +0100

    added .h comments

--- model.h
+++ model.h
@@ -3,14 +3,41 @@
 */
 
 
+/*
+ * Returns current time of the day in seconds
+ */
 int getTod();
+/*
+ * Returns time left of weekned (to Monday 0:00) in seconds. Returns zero on workdays
+ */
 int weekendTimeLeft();
+/*
+ * Parses and stores program arguments
+ */
 void parseArgs();
 
+/*
+ * Ticket class represents a support ticket written by customer
+ */
 class Ticket;
+/*
+ * Represents support worker. His job is to solve tickets send by customers.
+ */
 class SupportWorker;
+/*
+ * Represents backend developer. His job is to solve tickets that require some server maintenance.
+ */
 class BackendWorker;
+/*
+ * Represents an issue that customer needs help with. Issus is either solved in this class by livechat or delegated.
+ */
 class CustomerRequirement;
+/*
+ * Generates major breakdowns that influence multiple servers.
+ */
 class BreakdownGenerator;
+/*
+ * Generates common customer requirements.
+ */
 class Generator;
 
